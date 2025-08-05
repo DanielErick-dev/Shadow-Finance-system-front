d090628befd5bdd7953ca63e898ca73b53cecbdc
Daniel Erick <DanielErick.estudante@gmail.com>
2025-08-05 15:15:26 -0300

    feat(category): adds category editing and deletion

--- src/store/useCategoryStore.ts
+++ src/store/useCategoryStore.ts
@@ -9,6 +9,8 @@ export type CategoryState = {
     loading: boolean;
     fetchCategories: () => Promise<void>;
     addCategory: (CategoryData: NewCategoryData) => Promise<void>;
+    updateCategory: (categoryId: number, categoryData: NewCategoryData) => Promise<void>;
+    deleteCategory: (categoryId: number) => Promise<void>;
 }
 
 export const useCategoryStore = create<CategoryState>((set, get) => ({
@@ -32,6 +34,27 @@ export const useCategoryStore = create<CategoryState>((set, get) => ({
             success: 'Categoria Criada com Sucesso!',
             error: (err: any) => err.response?.data?.name?.[0] || 'Falha ao criar Categoria'
         });
+        await get().fetchCategories();
+    },
+    updateCategory: async (categoryId, categoryData) => {
+        const promise = api.patch(`/categories/${categoryId}/`, categoryData);
+        await toast.promise(promise, {
+            loading: 'Atualizando Categoria',
+            success: 'Categoria Atualizada com Sucesso',
+            error: 'Não foi possivel Atualizar a Categoria, verifique se ela já existe'
+        });
+        await get().fetchCategories();
+    },
+    deleteCategory: async (categoryId) => {
+        const promise = api.delete(`/categories/${categoryId}/`);
+        await toast.promise(promise, {
+            loading: 'Deletando Categoria...',
+            success: 'Categoria Deletada Com Sucesso!',
+            error: 'Não foi possivel Deletar a Categoria, Tente Novamente'
+        });
+        set(state => ({
+            categories: state.categories.filter(category => category.id !== categoryId)
+        }))
         await get().fetchCategories();
     },
 }))
